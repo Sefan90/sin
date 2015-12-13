584801ac880bd6b12363a1d0a0a7bba5b01769a0
Stefan <stevetv90@gmail.com>
2015-12-13 16:16:29 +0100

    Nya filer
    Nya filer och ska nu testa hur man skapar apk filer.
    Ska fixa fler banor och meny

---
+++ button.lua
@@ -0,0 +1,43 @@
+pressedx = 0
+pressedy = 0
+releasedx = 0
+releasedy = 0
+
+--Call drawbutton to use this function
+function clickbutton(buttons, x, y)
+		if x <= pressedx and x+buttons:getWidth() >= pressedx and y <= pressedy and y+buttons:getHeight() >= pressedy and x <= releasedx and x+buttons:getWidth() >= releasedx and y <= releasedy and y+buttons:getHeight() >= releasedy then
+			pressedx = 0
+			pressedy = 0
+			releasedx = 0
+			releasedy = 0
+			return true
+		end
+	return false
+end
+
+function drawbutton(buttons,text,x,y)
+	love.graphics.draw(buttons, x, y, 0)
+	if x <= love.mouse.getX() and x+buttons:getWidth() >= love.mouse.getX() and y <= love.mouse.getY() and y+buttons:getHeight() >= love.mouse.getY() then
+		love.graphics.print(text,20, 540)
+	end
+	if clickbutton(buttons, x, y) then
+		return true
+	end
+	return false
+end
+
+function love.mousepressed(x, y, button)
+   if button == "l" then
+   		releasedx = 0
+		releasedy = 0
+		pressedx = x
+		pressedy = y
+   end
+end
+
+function love.mousereleased(x, y, button)
+   if button == "l" then
+		releasedx = x
+		releasedy = y
+   end
+end
--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
+local sti = require "sti"
 require 'simple-slider'
+require 'button'
 function love.load()
+
+map = sti.new("bg")
 
 windowWidth = 480
 windowHeight = 320
+plus = love.graphics.newImage('assets/plus.png')
+minus = love.graphics.newImage('assets/minus.png')
+trans = love.graphics.newImage('assets/trans.png')
 
 if love.system.getOS() == "Android" then
 	local x,y = love.window.getDimensions()
@@ -18,51 +25,114 @@ love.window.setMode(windowWidth*scalex,windowHeight*scaley)
 x1 = love.math.random(2,52)/100
 x2 = love.math.random(2,52)/100
 x3 = love.math.random(2,52)/100
+x4 = love.math.random(2,52)/100
+x5 = love.math.random(2,52)/100
+x6 = love.math.random(2,52)/100
 
-y1 = love.math.random(-25,25)
-y2 = love.math.random(-25,25)
-y3 = love.math.random(-25,25)
+y1 = love.math.random(-50,50)
+y2 = love.math.random(-50,50)
+y3 = love.math.random(-50,50)
+y4 = love.math.random(-50,50)
+y5 = love.math.random(-50,50)
+y6 = love.math.random(-50,50)
 
-slider = newSlider(100, 100, 100, 0, -25, 25, setter, style)
-slider2 = newSlider(100, 140, 100, 0, -25, 25, setter, style)
-slider3 = newSlider(100, 180, 100, 0, -25, 25, setter, style)
+slider = newSlider(100, 40, 100, 0, -50, 50, setter, style)
+slider2 = newSlider(100, 88, 100, 0, -50, 50, setter, style)
+slider3 = newSlider(100, 136, 100, 0, -50, 50, setter, style)
+slider4 = newSlider(100, 184, 100, 0, -50, 50, setter, style)
+slider5 = newSlider(100, 232, 100, 0, -50, 50, setter, style)
+slider6 = newSlider(100, 280, 100, 0, -50, 50, setter, style)
 end
 
 function love.update(dt)
+	map:update(dt)
 	slider:update()
 	slider2:update()
+	slider2:update()
 	slider3:update()
+	slider4:update()
+	slider5:update()
+	slider6:update()
 end
 
 function love.draw(dt)
 	backgroundlines()
 	love.graphics.setColor(0, 255, 0)
 	love.graphics.setLineWidth(2)
-	love.graphics.print(slider:getValue(),100,80)
-	love.graphics.print(slider2:getValue(),100,120)
-	love.graphics.print(slider3:getValue(),100,160)
-	slider:draw()
-	slider2:draw()
-	slider3:draw()
-	for i = 1, 1500 do
-	--love.graphics.line(360+(math.sin(i*0.05-0.05)*100)+(math.sin(i-1)*20),i,360+(math.sin(i*0.05)*100)+(math.sin(i)*20),1+i)
-	love.graphics.line(360+(math.sin(i*x1-x1)*y1)-(math.sin(i*x1-x1)*slider:getValue())+(math.sin(i*x2-x2)*y2)-(math.sin(i*x2-x2)*slider2:getValue())+(math.sin(i*x3-x3)*y3)-(math.sin(i*x3-x3)*slider3:getValue()),i,360+(math.sin(i*x1)*y1)-(math.sin(i*x1)*slider:getValue())+(math.sin(i*x2)*y2)-(math.sin(i*x2)*slider2:getValue())+(math.sin(i*x3)*y3)-(math.sin(i*x3)*slider3:getValue()),1+i)
-	--love.graphics.line(360+(math.sin(i*x1-x1)*slider:getValue())+(math.cos(i*x2-x2)*slider2:getValue())+(math.sin(i*0.22-0.22)*slider3:getValue()),i,360+(math.sin(i*0.02)*slider:getValue())+(math.cos(i*0.12)*slider2:getValue())+(math.sin(i*0.22)*slider3:getValue()),1+i)
-	--love.graphics.line(360+(math.sin(i*0.02-0.02)*slider:getValue())+(math.cos(i*0.12-0.12)*slider2:getValue())+(math.sin(i*0.22-0.22)*slider3:getValue()),i,360+(math.sin(i*0.02)*slider:getValue())+(math.cos(i*0.12)*slider2:getValue())+(math.sin(i*0.22)*slider3:getValue()),1+i)
-	--love.graphics.line(360+(math.sin(i*slider3:getValue()-slider3:getValue())*slider:getValue())+(math.sin(i-1)*slider2:getValue()/10), i, 360+(math.sin(i*slider3:getValue())*slider:getValue())+(math.sin(i)*slider2:getValue()/10),1+(i))
-	--love.graphics.line(i, 300+(math.sin(i*f-f)*slider:getValue())+(math.sin(i-1)*slider2:getValue()/10), 1+(i), 300+(math.sin(i*f)*slider:getValue())+(math.sin(i)*slider2:getValue()/10))
-	--love.graphics.line( 100+(i), 100+(math.sin(i*f-f)*d), 101+(i), 100+(math.sin(i*f)*d))
-	--love.graphics.line( 100+(i), 400+(math.cos(i*f-f)*d), 101+(i), 400+(math.cos(i*f)*d))
-	--love.graphics.line( 100+(i), 300+(math.cos(i*0.01-0.01)*100)-(math.sin(i*f)*d), 101+(i), 300+(math.sin(i*0.01)*100)-(math.sin(i*f)*d))
-	end
+	levels(1)
+	map:draw()
+	sliders()
 end
 
 function love.keypressed(key)
 	lastkey = key
 end
 
+function levels(level)
+	if level == 1 then
+		for i = 1, 1500 do
+			--love.graphics.line(360+(math.sin(i*x1-x1)*y1)-(math.sin(i*x1-x1)*value1)+(math.sin(i*x2-x2)*y2)-(math.sin(i*x2-x2)*value2)+(math.sin(i*x3-x3)*y3)-(math.sin(i*x3-x3)*value3),i,360+(math.sin(i*x1)*y1)-(math.sin(i*x1)*value1)+(math.sin(i*x2)*y2)-(math.sin(i*x2)*value2)+(math.sin(i*x3)*y3)-(math.sin(i*x3)*value3),1+i)
+			--love.graphics.line(360+(math.sin(i*x1-x1)*y1)-(math.sin(i*x1-x1)*slider:getValue())+(math.sin(i*x2-x2)*y2)-(math.sin(i*x2-x2)*slider2:getValue())+(math.sin(i*x3-x3)*y3)-(math.sin(i*x3-x3)*slider3:getValue()),i,360+(math.sin(i*x1)*y1)-(math.sin(i*x1)*slider:getValue())+(math.sin(i*x2)*y2)-(math.sin(i*x2)*slider2:getValue())+(math.sin(i*x3)*y3)-(math.sin(i*x3)*slider3:getValue()),1+i)
+			love.graphics.line(360+(math.sin(i*x1-x1)*y1)-(math.sin(i*x1-x1)*slider:getValue())+(math.sin(i*x2-x2)*y2)-(math.sin(i*x2-x2)*slider2:getValue())+(math.sin(i*x3-x3)*y3)-(math.sin(i*x3-x3)*slider3:getValue())+(math.sin(i*x4-x4)*y4)-(math.sin(i*x4-x4)*slider4:getValue())+(math.sin(i*x5-x5)*y5)-(math.sin(i*x5-x5)*slider5:getValue())+(math.sin(i*x6-x6)*y6)-(math.sin(i*x6-x6)*slider6:getValue())
+			,i,360+(math.sin(i*x1)*y1)-(math.sin(i*x1)*slider:getValue())+(math.sin(i*x2)*y2)-(math.sin(i*x2)*slider2:getValue())+(math.sin(i*x3)*y3)-(math.sin(i*x3)*slider3:getValue())+(math.sin(i*x4)*y4)-(math.sin(i*x4)*slider4:getValue())+(math.sin(i*x5)*y5)-(math.sin(i*x5)*slider5:getValue())+(math.sin(i*x6)*y6)-(math.sin(i*x6)*slider6:getValue()),1+i)
+		end
+	end
+end
+
+function sliders()
+	love.graphics.setColor(0, 0, 0)
+	if drawbutton(trans,"",16,24) == true then
+		slider = newSlider(100, 40, 100, slider:getValue()-1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",150,24) == true then
+		slider = newSlider(100, 40, 100, slider:getValue()+1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",16,72) == true then
+		slider2 = newSlider(100, 88, 100, slider2:getValue()-1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",150,72) == true then
+		slider2 = newSlider(100, 88, 100, slider2:getValue()+1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",16,120) == true then
+		slider3 = newSlider(100, 136, 100, slider3:getValue()-1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",150,120) == true then
+		slider3 = newSlider(100, 136, 100, slider3:getValue()+1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",16,168) == true then
+		slider4 = newSlider(100, 184, 100, slider4:getValue()-1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",150,168) == true then
+		slider4 = newSlider(100, 184, 100, slider4:getValue()+1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",16,216) == true then
+		slider5 = newSlider(100, 232, 100, slider5:getValue()-1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",150,216) == true then
+		slider5 = newSlider(100, 232, 100, slider5:getValue()+1, -50, 50, setter, style)
+	end
+		if drawbutton(trans,"",16,264) == true then
+		slider6 = newSlider(100, 280, 100, slider6:getValue()-1, -50, 50, setter, style)
+	end
+	if drawbutton(trans,"",150,264) == true then
+		slider6 = newSlider(100, 280, 100, slider6:getValue()+1, -50, 50, setter, style)
+	end
+	love.graphics.print(math.floor(slider:getValue()),200,32,math.pi/2)
+	love.graphics.print(math.floor(slider2:getValue()),200,80,math.pi/2)
+	love.graphics.print(math.floor(slider3:getValue()),200,128,math.pi/2)
+	love.graphics.print(math.floor(slider4:getValue()),200,176,math.pi/2)
+	love.graphics.print(math.floor(slider5:getValue()),200,224,math.pi/2)
+	love.graphics.print(math.floor(slider6:getValue()),200,272,math.pi/2)
+	slider:draw()
+	slider2:draw()
+	slider3:draw()
+	slider4:draw()
+	slider5:draw()
+	slider6:draw()
+end
+
 function backgroundlines()
-	love.graphics.setColor(125, 125, 125)
+	love.graphics.setColor(0, 125, 0,125)
 	love.graphics.setLineWidth(1)
 	love.graphics.line(240,0,240,320)
 	love.graphics.line(250,0,250,320)
